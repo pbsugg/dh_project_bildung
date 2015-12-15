1ea0ceb39105574298659630cdd48e600ead7948
Philip Sugg <pbensugg@gmail.com>
2015-12-14 23:15:52 -0600

    add initial solution

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ This project is an experiment to test the capabilities and resources of UChicago
 The German word "Bildung" is one of the richer and harder-to-define words in the German lexicon.  The modern history of the word begins around 1750.  In general, it refers to a person's particular journey of self-culivation and self-directed development.  It is built upon strongly individualistic ideals, particularly the notion that each of us has a unique potential that requires us to chart our own particular path in life.  To speak of a person's "Bildung" is to describe his or her own journey of growth, maturation, and self-discovery.   
 Around or before 1750, Bildung was usually used in a religious context, but since then has become secularized and gained many variants. In the 20th century, the idea of Bildung has been tied to problems as specific and concerte as the acquisition of useful vocational skills and as idealistic as cultural fluency and assimilation.  Because of its many uses, the term is uniquely well-suited to an analysis of its changing meaning and contexts over time.  This is what I propose to test using the HathiTrust database
 
-# About HathiTrust and UChicago's Research Computing Center #
+## About HathiTrust and UChicago's Research Computing Center ##
 
-# Release 1 #
+## Release 1 ##
 
 The primary goal of the first release is to gather the raw information on which a more sophisticated analysis (e.g., natural language processing techniques) can be run. Over the entire HathiTrust database of German-language sources, I will find all instances where "Bildung" (and variations on the word) appears along with a 30-word "window" (15 words on each side) of context.  Other metadata collected about each occurrence will include the volume number and page number.
--- basic_context.py
+++ basic_context.py
@@ -1,2 +1,68 @@
-import os
+# program to run in python 3.5
+
 import zipfile
+from zipfile import ZipFile
+import codecs
+import nltk
+# from nltk.corpus import stopwords
+import string
+import csv
+
+# assuming for the moment that my target file is a series of zipped directories, organized by page, as documented here
+# https://www.hathitrust.org/documents/HTRC-UnCamp2012-themed-htrc-api-overview-1.pdf
+
+# "sample_volumes"are random snippets (with references to Bildung sprinkled throughout) from Humboldt's "Briefe an eine Freundin"
+# http://onlinebooks.library.upenn.edu/webbin/gutbook/lookup?num=21801
+
+target_file = zipfile.ZipFile('sample_volumes.zip', mode='r')
+iterable_file_list = target_file.infolist()
+
+# not doing stopwords for now
+# stop = stopwords.words('german')
+
+# occurrences is the array where I will store the results
+occurrences = []
+
+for text_file in iterable_file_list:
+    name = text_file.filename
+    # escaping directories, only want the page numbers themselves
+    if name.endswith('.txt'):
+        result = codecs.open(name, 'r', 'utf-8')
+        content = result.read()
+        tokenized = nltk.word_tokenize(content)
+        # deciding not to remove stopwords for now, could change
+        # for token in tokenized:
+        #     if token in stop:
+        #         tokenized.remove(token)
+
+        # downcase the result
+        cleaned_up_file = [token.lower() for token in tokenized]
+        # removing punctuation words for now
+        for word in cleaned_up_file:
+            if word in string.punctuation:
+                cleaned_up_file.remove(word)
+        file_index_length = len(cleaned_up_file) - 1
+        for center_index, word in enumerate(cleaned_up_file):
+            # also searching for variants of bildung
+            if "bildung" in word:
+                # main logic to calculate the context on either side, want to make sure I don't go out of range
+                if (center_index - 15 < 0):
+                    start_index = 0
+                else:
+                    start_index = center_index - 15
+                if (center_index + 15 > file_index_length):
+                    end_index = file_index_length
+                else:
+                    end_index = center_index + 15
+                # context_slice is the finished array context slice for each occurrence
+                context_slice = cleaned_up_file[start_index:end_index]
+                occurrences.append(context_slice)
+
+# write result to a csv
+# todo: may need to adjust write/row format but you get the idea.
+# todo: no year data right now, because I don't know where I'll get it
+# See "results.csv" for how this comes out
+
+with open('result.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(occurrences)
